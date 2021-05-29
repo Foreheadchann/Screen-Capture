0c4b55df06291d2fba7fb09630a20cd7a34418f6
Foreheadchann <foreheadchann@googlemail.com>
2021-05-29 19:18:15 +0300

    Updated Window and RenderThread
    Added settings to Screen Capture

--- src/main/java/me/hideri/render/Window.java
+++ src/main/java/me/hideri/render/Window.java
@@ -12,25 +12,38 @@ public class Window
 {
     public final ScreenImageCapture imageCapture = new ScreenImageCapture();
     private final List<BufferedImage> frameBuffer = new ArrayList<>();
+
     private final BufferedImage cursor = imageCapture.getImage("assets/Cursor.png");
     public BufferedImage latestImage;
+
+    private int setting_x, setting_y, setting_width, setting_height;
     private int x, y, width, height;
     private int mouseX, mouseY;
+
+    public JFrame settingWindow;
     public JFrame window;
+
+    private JTextField widthField;
+    private JTextField heightField;
+    private JButton setSize;
 
     private boolean pressed;
 
-    public Window(int width, int height)
+    public Window(int x, int y, int width, int height, int setting_x, int setting_y, int setting_width, int setting_height)
     {
         this.width = width;
         this.height = height;
-        this.x = 25;
-        this.y = 25;
+        this.x = x;
+        this.y = y;
+        this.setting_width = setting_width + 16;
+        this.setting_height = setting_height + 39;
+        this.setting_x = setting_x;
+        this.setting_y = setting_y;
     }
 
     public void initWindow()
     {
-        window = new JFrame();
+        window = new JFrame("Screen Capture");
         window.setResizable(false);
         window.setBounds(x, y, width, height);
         window.setUndecorated(true);
@@ -39,6 +52,45 @@ public class Window
         window.setVisible(true);
         window.setAlwaysOnTop(true);
         this.initMouseEvents();
+    }
+
+    public void initSettingWindow()
+    {
+        settingWindow = new JFrame("Settings");
+        settingWindow.setResizable(false);
+        settingWindow.setBounds(setting_x, setting_y, setting_width, setting_height);
+        settingWindow.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        settingWindow.setBackground(new Color(0, 0, 0));
+        settingWindow.repaint();
+        settingWindow.setLayout(null);
+        settingWindow.setVisible(true);
+        this.initSettings();
+        this.initSettingEvents();
+    }
+
+    public void initSettings()
+    {
+        widthField = new JTextField(String.valueOf(this.getWidth()));
+        heightField = new JTextField(String.valueOf(this.getHeight()));
+        widthField.setBounds(65, 5, settingWindow.getContentPane().getWidth() - 70, 25);
+        heightField.setBounds(65, 35, settingWindow.getContentPane().getWidth() - 70, 25);
+        widthField.setVisible(true);
+        heightField.setVisible(true);
+        settingWindow.getContentPane().add(widthField);
+        settingWindow.getContentPane().add(heightField);
+        settingWindow.getContentPane().revalidate();
+        settingWindow.getContentPane().repaint();
+
+        setSize = new JButton("Set Size");
+        setSize.setBounds(5, settingWindow.getContentPane().getHeight() - 25, settingWindow.getContentPane().getWidth() - 10, 20);
+        setSize.setVisible(true);
+        settingWindow.getContentPane().add(setSize);
+    }
+
+    public void drawSettingStrings()
+    {
+        this.drawStringOnScreen(settingWindow.getContentPane().getGraphics(), "Width", 5, 21);
+        this.drawStringOnScreen(settingWindow.getContentPane().getGraphics(), "Height", 5, 51);
     }
 
     public void initMouseEvents()
@@ -64,10 +116,49 @@ public class Window
         });
     }
 
+    public void initSettingEvents()
+    {
+        widthField.addActionListener(event ->
+        {
+            String width = widthField.getText();
+
+            if(width.equals("") || width.charAt(0) == ' ')
+            {
+                return;
+            }
+
+            this.setPos(this.x, this.y, Integer.parseInt(width), this.height);
+        });
+
+        heightField.addActionListener(event ->
+        {
+            String height = heightField.getText();
+
+            if(height.equals("") || height.charAt(0) == ' ')
+            {
+                return;
+            }
+
+            this.setPos(this.x, this.y, this.width, Integer.parseInt(height));
+        });
+
+        setSize.addActionListener(event ->
+        {
+            String width = widthField.getText();
+            String height = heightField.getText();
+
+            if(height.equals("") || height.charAt(0) == ' ' || width.equals("") || width.charAt(0) == ' ')
+            {
+                return;
+            }
+
+            this.setPos(this.x, this.y, Integer.parseInt(width), Integer.parseInt(height));
+        });
+    }
+
     public void drawCursor(Graphics graphics, int x, int y)
     {
-        this.setMousePos((int) Math.round(x / ((double) this.width / (double) this.height)), (int) Math.round(y / ((double) this.width / (double) this.height)));
-        System.out.println("@" + mouseX + "," + mouseY);
+        this.setMousePos((int) (x / ((double) window.getWidth() / (double) window.getHeight())), (int) (y / ((double) window.getWidth() / (double) window.getHeight())));
         final Graphics2D g2d = (Graphics2D) graphics;
         graphics.drawImage(cursor, mouseX, mouseY, null);
         g2d.dispose();
@@ -120,9 +211,8 @@ public class Window
 
     public void drawStringOnScreen(Graphics graphics, String text, int x, int y)
     {
-        final Graphics2D g2d = (Graphics2D) graphics;
-        g2d.drawString(text, x, y);
-        g2d.dispose();
+        graphics.setFont(new Font("Arial", Font.PLAIN, 14));
+        graphics.drawString(text, x, y);
         graphics.dispose();
     }
 
@@ -148,6 +238,86 @@ public class Window
         this.height = height;
     }
 
+    public int getSetting_x()
+    {
+        return setting_x;
+    }
+
+    public void setSetting_x(int setting_x)
+    {
+        this.setting_x = setting_x;
+    }
+
+    public int getSetting_y()
+    {
+        return setting_y;
+    }
+
+    public void setSetting_y(int setting_y)
+    {
+        this.setting_y = setting_y;
+    }
+
+    public int getSetting_width()
+    {
+        return setting_width;
+    }
+
+    public void setSetting_width(int setting_width)
+    {
+        this.setting_width = setting_width;
+    }
+
+    public int getSetting_height()
+    {
+        return setting_height;
+    }
+
+    public void setSetting_height(int setting_height)
+    {
+        this.setting_height = setting_height;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
+    public int getMouseX()
+    {
+        return mouseX;
+    }
+
+    public void setMouseX(int mouseX)
+    {
+        this.mouseX = mouseX;
+    }
+
+    public int getMouseY()
+    {
+        return mouseY;
+    }
+
+    public void setMouseY(int mouseY)
+    {
+        this.mouseY = mouseY;
+    }
+
     public void setPos(int x, int y, int width, int height)
     {
         window.setBounds(x, y, width, height);
@@ -155,6 +325,15 @@ public class Window
         this.y = y;
         this.width = width;
         this.height = height;
+    }
+
+    public void setSettingPos(int x, int y, int width, int height)
+    {
+        settingWindow.setBounds(x, y, width, height);
+        this.setting_x = x;
+        this.setting_y = y;
+        this.setting_width = width;
+        this.setting_height = height;
     }
 
     public void setMousePos(int x, int y)
--- src/main/java/me/hideri/thread/RenderThread.java
+++ src/main/java/me/hideri/thread/RenderThread.java
@@ -6,13 +6,14 @@ import java.awt.*;
 
 public class RenderThread
 {
-    public static final Window window = new Window(1280, 720);
+    public static final Window window = new Window(350, 25, 1280, 720, 0, 0, 300, 500);
     public static final long frame_delay = 0;
 
     public void initRenderThread()
     {
         final Thread renderThread = new Thread(new Render());
         renderThread.setPriority(Thread.MAX_PRIORITY);
+        window.initSettingWindow();
         window.initWindow();
         renderThread.start();
     }
@@ -22,6 +23,7 @@ public class RenderThread
         final Point mouseLoc = MouseInfo.getPointerInfo().getLocation();
         window.drawImage(window.window.getGraphics(), true);
         window.drawCursor(window.window.getGraphics(), mouseLoc.x, mouseLoc.y);
+        window.drawSettingStrings();
     }
 
     public class Render implements Runnable
